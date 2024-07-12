3ee7995eb7da171aa1343282e76ea983eef31d7f
Danny THOMAS <133696646+Psyko44@users.noreply.github.com>
2024-07-12 17:42:19 +0200

    Update Vite and Vue config files

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/' // Remplacez par le nom de votre repository GitHub
+      ? '/CyberFeed' // Remplacez par le nom de votre repository GitHub
       : '/'
   }
   
