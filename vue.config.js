8ee131ade86bdee04417e6a71ee552246fb539ba
Danny THOMAS <133696646+Psyko44@users.noreply.github.com>
2024-07-12 17:26:57 +0200

    Add build files and vue.config.js for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/CyberFeed/' // Remplacez par le nom de votre repository GitHub
+      ? '/' // Remplacez par le nom de votre repository GitHub
       : '/'
   }
   
