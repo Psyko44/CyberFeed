5859a3c83cd7cef3df90025d7a5fcdc432afc50d
Danny THOMAS <133696646+Psyko44@users.noreply.github.com>
2024-07-12 18:24:32 +0200

    vite change

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: '/CyberFeed/', // Assurez-vous que ce chemin correspond à votre repository
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': '/src',
-    },
-  },
-});
+      '@': '/src'
+    }
+  }
+})
